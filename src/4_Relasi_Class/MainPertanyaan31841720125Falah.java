732090d7a9e447ab9e20360badfe29ebc58bb7c7
mutankentang <52905030+mutankentang@users.noreply.github.com>
2019-09-24 12:36:50 +0700

    Add files via upload
    penambahan java untuk pertanyaan percobaan3

---
+++ src/4_Relasi_Class/MainPertanyaan31841720125Falah.java
@@ -0,0 +1,10 @@
+package pbojsempat;
+public class MainPertanyaan31841720125Falah {
+    public static void main(String[] args) {
+        Pegawai1841720125Falah masinis = new Pegawai1841720125Falah("1234","Spongebob");
+        Pegawai1841720125Falah asisten = new Pegawai1841720125Falah("1567","Patrick");
+        KeretaApi1841720125Falah keretaApi = new KeretaApi1841720125Falah("Gaya Baru", "Bisnis",masinis, asisten);
+        System.out.println(keretaApi.info());
+    }
+    
+}
